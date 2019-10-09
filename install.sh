b0ec4af594371d6c967807b1a549fd25f3d5eec2
Pruthvi <pmagundu@amd.com>
2019-10-09 12:25:17 -0700

    RocSPARSE Changes for relocatable installation  - Updated the lib so version, so name spec and RPATH.  - Updated ld.so.conf entry  - Changes are done as part of the experiment for FBA-29/69
    Signed-off-by: Pruthvi <pmagundu@amd.com>

--- install.sh
+++ install.sh
@@ -113,7 +113,7 @@ install_packages( )
   fi
 
   # dependencies needed for library and clients to build
-  local library_dependencies_ubuntu=( "make" "cmake-curses-gui" "hip_hcc" "pkg-config" "libnuma1" "rocprim")
+  local library_dependencies_ubuntu=( "make" "cmake" "hip_hcc" "pkg-config" "libnuma1" "rocprim")
   local library_dependencies_centos=( "epel-release" "make" "cmake3" "hip_hcc" "gcc-c++" "rpm-build" "numactl-libs" "rocprim" )
   local library_dependencies_fedora=( "make" "cmake" "hip_hcc" "gcc-c++" "libcxx-devel" "rpm-build" "numactl-libs" "rocprim" )
 
@@ -284,7 +284,7 @@ fi
 
 # We append customary rocm path; if user provides custom rocm path in ${path}, our
 # hard-coded path has lesser priority
-export PATH=${PATH}:/opt/rocm/bin
+export PATH=${PATH}:/opt/rocm-2.7.3/bin
 
 pushd .
   # #################################################
@@ -308,7 +308,7 @@ pushd .
   fi
 
   # cpack
-  cmake_common_options="${cmake_common_options} -DCPACK_SET_DESTDIR=OFF -DCPACK_PACKAGING_INSTALL_PREFIX=/opt/rocm"
+  cmake_common_options="${cmake_common_options} -DCPACK_SET_DESTDIR=OFF -DCPACK_PACKAGING_INSTALL_PREFIX=/opt/rocm-2.7.3"
 
   # compiler
   compiler="hipcc"
